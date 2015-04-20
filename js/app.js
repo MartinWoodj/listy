5fe4570d3c9eaf3deaa1b9c2ce7d2d9c0b6f6879
Joseph Wood <martinwoodj@me.com>
2015-04-20 18:16:33 -0400

    Added enter key to add item code

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	// Enter key adds items
+	$('#item').keyup(function(e){
+		if(event.keyCode == 13) {
+			$('#add').click();
+		};
+		
+	});
+
+	// adding list items
+
+});
+
+
+/*
+- Type new item into text box - DONE
+- Hit "enter" for item to be added to list
+- List item will display with checkbox left of item - DONE
+- Clicking inside checkbox will strike through item
+- Strike through item will then be dropped to the bottom of the list
+- List item that is completed with a strike through and a checkmark is also greyed out
+- Unchecking a checked item moves the list item back to the top of the list
+- *Figure out how to permanently remove an item from the list
+*/
