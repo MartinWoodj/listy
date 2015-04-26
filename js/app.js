a228670ef7056802d3bf6d947738c88ef2d7f818
Joseph Wood <josephwood@Josephs-MacBook-Pro.local>
2015-04-26 12:21:12 -0400

    Added item add and checkbox

--- js/app.js
+++ js/app.js
@@ -2,20 +2,26 @@ $(document).ready(function(){
 	// Enter key adds items
 	$('#item').keyup(function(e){
 		if(event.keyCode == 13) {
-			$('#add').click();
+// 			$('#add').click();
+			inputItem = $('#item').val();
+			console.log("You pressed enter!");
+			$('#list').prepend("<li>" + inputItem + "</li>");
+			$('<input type="checkbox" value="1" />').prependTo('li');
 		};
 		
 	});
 
 	// adding list items
-
+	
 });
+
+
 
 
 /*
 - Type new item into text box - DONE
 - Hit "enter" for item to be added to list
-- List item will display with checkbox left of item - DONE
+- List item will display with checkbox left of item
 - Clicking inside checkbox will strike through item
 - Strike through item will then be dropped to the bottom of the list
 - List item that is completed with a strike through and a checkmark is also greyed out
