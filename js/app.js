4decd71a2a1469f11770f3dcd1294972247fb681
Joseph Wood <martinwoodj@me.com>
2015-04-28 19:17:40 -0400

    Items dropping under HR after checked

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
             if($(this).is(":checked")){
                 console.log("Checkbox is checked.");
                 $(".listItem").removeClass("listItem").addClass("checked");
+                $('#itemDone').prepend('<li class="listItem"><input type="checkbox" />' + inputItem + '</li>');
             	}
             else if($(this).is(":not(:checked)")){
                 console.log("Checkbox is unchecked.");
@@ -29,9 +30,9 @@ $(document).ready(function(){
 
 /*
 - Type new item into text box - DONE
-- Hit "enter" for item to be added to list
-- List item will display with checkbox left of item
-- Clicking inside checkbox will strike through item
+- Hit "enter" for item to be added to list - DONE
+- List item will display with checkbox left of item - DONE
+- Clicking inside checkbox will strike through item - DONE
 - Strike through item will then be dropped to the bottom of the list
 - List item that is completed with a strike through and a checkmark is also greyed out
 - Unchecking a checked item moves the list item back to the top of the list
